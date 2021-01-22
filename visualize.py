7b39cd80b529f405b8cd2cb2e3e984e73988b9bc
Moulishankar10 <moulishankarmr@gmail.com>
2021-01-22 19:20:00 +0530

    Imported data

---
+++ visualize.py
@@ -0,0 +1,16 @@
+# INDIAN CURRENCY VALUE PREDICTION
+
+# DEVELOPED BY: 
+# MOULISHANKAR M R
+
+# IMPORTING MODULES
+import numpy as np
+import pandas as pd
+from datetime import date
+from tensorflow import keras
+from tensorflow.keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
+
+# IMPORTING DATA
+data = pd.read_csv("price.csv")
+
