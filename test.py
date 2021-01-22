c3188197f53c43ead2e7595d9dfd6eabe747cdd9
Moulishankar10 <moulishankarmr@gmail.com>
2021-01-22 18:57:50 +0530

    Preprocessed data

--- test.py
+++ test.py
@@ -13,3 +13,20 @@ from sklearn.preprocessing import MinMaxScaler
 
 # IMPORTING DATA
 data = pd.read_csv("price.csv")
+
+# PREPROCESSING DATA
+x = []
+initial_str = data["Date"][0]
+initial = date(int(initial_str[-4:]),int(initial_str[3:5]),int(initial_str[:2]))
+ 
+for i in range(len(data["Date"])):
+    final_str = data["Date"][i]
+    final = date(int(final_str[-4:]),int(final_str[3:5]),int(final_str[:2]))
+    diff = (final - initial).days
+    x.append(diff)
+
+y = data["Price"].values
+
+# RESHAPING THE DATA
+x = np.reshape(x, (-1,1))
+y = np.reshape(y, (-1,1))
